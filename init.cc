a44b16a4052d2793d3af8e2eef37bd78d5d53157
David Drysdale <drysdale@google.com>
2013-09-10 11:56:44 +0100

    Add a few init tests

--- init.cc
+++ init.cc
@@ -1,9 +1,37 @@
-#include "gtest/gtest.h"
-#include "globals.h"
+#include "pkcs11test.h"
 
 using namespace std;  // So sue me
 
-TEST(LibraryInit, Simple) {
-  EXPECT_EQ(CKR_OK, g_fns->C_Initialize(NULL_PTR));
-  EXPECT_EQ(CKR_OK, g_fns->C_Finalize(NULL_PTR));
+// Explicitly test Initialize/Finalize.
+TEST(Init, Simple) {
+  EXPECT_CKR_OK(g_fns->C_Initialize(NULL_PTR));
+  EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
 }
+
+TEST(Init, InitArgs) {
+  CK_C_INITIALIZE_ARGS init_args;
+  init_args.CreateMutex = 0;
+}
+
+
+// From here on, wrap Initialize/Finalize in a fixture.
+class InitTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    EXPECT_CKR_OK(g_fns->C_Initialize(NULL_PTR));
+  }
+  virtual void TearDown() {
+    EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
+  }
+};
+
+TEST_F(InitTest, GetInfo) {
+  CK_INFO info = {0};
+  EXPECT_CKR_OK(g_fns->C_GetInfo(&info));
+  cout << info_description(&info) << endl;
+}
+
+TEST_F(InitTest, FailedTermination) {
+  EXPECT_EQ(CKR_ARGUMENTS_BAD, g_fns->C_Finalize((void *)1));
+}
+

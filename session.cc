e7b8062f4e9fa99af5201b4f7fadb97b5bfb7b5c
David Drysdale <drysdale@google.com>
2013-09-10 14:38:45 +0100

    Add tests for different session types

--- session.cc
+++ session.cc
@@ -3,7 +3,19 @@
 
 using namespace std;  // So sue me
 
+TEST_F(PKCS11Test, ParallelSessionUnsupported) {
+  // No CKF_SERIAL_SESSION => not supported
+  CK_SESSION_HANDLE session;
+  EXPECT_EQ(CKR_SESSION_PARALLEL_NOT_SUPPORTED, g_fns->C_OpenSession(g_slot_id, 0, NULL_PTR, NULL_PTR, &session));
+}
+
 TEST_F(ReadOnlySessionTest, SessionInfo) {
+  CK_SESSION_INFO session_info;
+  EXPECT_CKR_OK(g_fns->C_GetSessionInfo(session_, &session_info));
+  cout << session_info_description(&session_info) << endl;
+}
+
+TEST_F(ReadWriteSessionTest, SessionInfo) {
   CK_SESSION_INFO session_info;
   EXPECT_CKR_OK(g_fns->C_GetSessionInfo(session_, &session_info));
   cout << session_info_description(&session_info) << endl;

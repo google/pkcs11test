6661ad29ff5545d5ff3c3a5fd03d0838e4e86eb9
David Drysdale <drysdale@google.com>
2013-09-10 11:49:14 +0100

    fix function name to match code

--- pkcs11-describe.h
+++ pkcs11-describe.h
@@ -6,7 +6,7 @@
 
 #include <string>
 
-std::string retval_name(CK_RV val);
+std::string rv_name(CK_RV val);
 std::string key_type_name(CK_KEY_TYPE val);
 std::string mechanism_type_name(CK_MECHANISM_TYPE val);
 std::string certificate_type_name(CK_CERTIFICATE_TYPE val);

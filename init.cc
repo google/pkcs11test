c0f067afcf224021a2c729f8dd84501e1ba1826b
David Drysdale <drysdale@google.com>
2013-09-10 12:21:39 +0100

    Add more init tests

--- init.cc
+++ init.cc
@@ -8,11 +8,44 @@ TEST(Init, Simple) {
   EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
 }
 
-TEST(Init, InitArgs) {
-  CK_C_INITIALIZE_ARGS init_args;
-  init_args.CreateMutex = 0;
+TEST(Init, UnexpectedFinalize) {
+  EXPECT_EQ(CKR_CRYPTOKI_NOT_INITIALIZED, g_fns->C_Finalize(NULL_PTR));
 }
 
+TEST(Init, InitArgsBadReserved) {
+  CK_C_INITIALIZE_ARGS init_args = {0};
+  init_args.pReserved = (void*)1;
+  EXPECT_EQ(CKR_ARGUMENTS_BAD, g_fns->C_Initialize(&init_args));
+}
+
+TEST(Init, InitArgsNoNewThreads) {
+  CK_C_INITIALIZE_ARGS init_args = {0};
+  init_args.flags = CKF_LIBRARY_CANT_CREATE_OS_THREADS;
+  CK_RV rv = g_fns->C_Initialize(&init_args);
+  if (rv == CKR_OK) {
+    cout << "Library can cope without creating OS threads" << endl;
+    EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
+  } else {
+    cout << "Library needs to be able to create OS threads" << endl;
+    EXPECT_EQ(CKR_NEED_TO_CREATE_THREADS, rv);
+  }
+}
+
+TEST(Init, InitArgsNoLock) {
+  CK_C_INITIALIZE_ARGS init_args = {0};
+  EXPECT_CKR_OK(g_fns->C_Initialize(&init_args));
+  EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
+}
+
+TEST(Init, InitArgsInternalLocks) {
+  CK_C_INITIALIZE_ARGS init_args = {0};
+  init_args.flags = CKF_OS_LOCKING_OK;
+  // Expect the library to use OS threading primitives
+  EXPECT_CKR_OK(g_fns->C_Initialize(&init_args));
+  EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
+}
+
+// TODO(drysdale): add the other two cases (function pointers supplied, flag set+not-set)
 
 // From here on, wrap Initialize/Finalize in a fixture.
 class InitTest : public ::testing::Test {
@@ -24,6 +57,10 @@ protected:
     EXPECT_CKR_OK(g_fns->C_Finalize(NULL_PTR));
   }
 };
+
+TEST_F(InitTest, NestedFail) {
+  EXPECT_EQ(CKR_CRYPTOKI_ALREADY_INITIALIZED, g_fns->C_Initialize(NULL_PTR));
+}
 
 TEST_F(InitTest, GetInfo) {
   CK_INFO info = {0};

054c1310a87ca695e432f7af7a4721f8a3804148
David Drysdale <drysdale@google.com>
2013-09-12 14:29:16 +0100

    Sets indicating which mechanisms have what function

--- globals.h
+++ globals.h
@@ -5,6 +5,8 @@
 #include "pkcs11-env.h"
 #include <pkcs11.h>
 
+#include <set>
+
 // Set of function pointers holding PKCS#11 implementation.
 extern CK_FUNCTION_LIST_PTR g_fns;
 // Slot to perform tests against.
@@ -17,5 +19,21 @@ extern const char* g_user_pin;
 extern const char* g_so_pin;
 // Whether the token has a random number generator.
 extern bool g_rng;
+
+// PKCS#11 mechanisms for encrypt/decrypt.
+extern std::set<CK_MECHANISM_TYPE> encrypt_decrypt_mechanisms;
+// PKCS#11 mechanisms for sign/verify.
+extern std::set<CK_MECHANISM_TYPE> sign_verify_mechanisms;
+// PKCS#11 mechanisms for sign/verify-recover.
+extern std::set<CK_MECHANISM_TYPE> sign_verify_recover_mechanisms;
+// PKCS#11 mechanisms for digest generation.
+extern std::set<CK_MECHANISM_TYPE> digest_mechanisms;
+// PKCS#11 mechanisms for key generation.
+extern std::set<CK_MECHANISM_TYPE> generate_mechanisms;
+// PKCS#11 mechanisms for wrap/unwrap.
+extern std::set<CK_MECHANISM_TYPE> wrap_unwrap_mechanisms;
+// PKCS#11 mechanisms for derive.
+extern std::set<CK_MECHANISM_TYPE> derive_mechanisms;
+
 
 #endif  // GLOBALS_H

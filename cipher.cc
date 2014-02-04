7a4109c91a0e30fdfd632e40d15c9241babd2158
David Drysdale <drysdale@google.com>
2014-02-04 11:56:34 +0000

    Add more cipher tests
     - Part-by-part encryption
     - Error cases

--- cipher.cc
+++ cipher.cc
@@ -8,6 +8,7 @@ namespace test {
 
 class SecretKeyTest : public ReadOnlySessionTest {
  public:
+  static const int kNumBlocks = 4;
   SecretKeyTest(CK_MECHANISM_TYPE keygen_mechanism,
                 CK_MECHANISM_TYPE mode,
                 int blocksize,
@@ -17,34 +18,30 @@ class SecretKeyTest : public ReadOnlySessionTest {
       key_(session_, attrs_, keygen_mechanism, keylen),
       mode_(mode),
       blocksize_(blocksize),
-      emits_iv_(emits_iv) {}
+      emits_iv_(emits_iv),
+      iv_(randmalloc(blocksize_)),
+      plaintext_(randmalloc(kNumBlocks * blocksize_)),
+      mechanism_({mode_, (emits_iv_ ? iv_.get() : NULL_PTR), (emits_iv_ ? blocksize_ : 0)}) {
+    if (g_verbose && emits_iv_) cout << "IV: " << hex_data(iv_.get(), blocksize_) << endl;
+    if (g_verbose) cout << "PT: " << hex_data(plaintext_.get(), kNumBlocks * blocksize_) << endl;
+  }
 
   void TestEncryptDecrypt() {
-    unique_ptr<CK_BYTE, freer> iv(randmalloc(blocksize_));
-    if (g_verbose && emits_iv_) cout << "IV: " << hex_data(iv.get(), blocksize_) << endl;
-
-    CK_ULONG plaintext_len = 4 * blocksize_;
-    unique_ptr<CK_BYTE, freer> plaintext(randmalloc(plaintext_len));
-    if (g_verbose) cout << "PT: " << hex_data(plaintext.get(), plaintext_len) << endl;
-
     // First encrypt the data.
-    CK_MECHANISM mechanism = {mode_,
-                              (emits_iv_ ? iv.get() : NULL_PTR),
-                              (emits_iv_ ? blocksize_ : 0)};
-    CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism, key_.handle());
+    CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
     EXPECT_CKR_OK(rv);
     if (rv != CKR_OK) return;
 
     CK_BYTE ciphertext[1024];
     CK_ULONG ciphertext_len = sizeof(ciphertext);
-    rv = g_fns->C_Encrypt(session_, plaintext.get(), plaintext_len, ciphertext, &ciphertext_len);
+    rv = g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, ciphertext, &ciphertext_len);
     EXPECT_CKR_OK(rv);
-    EXPECT_EQ(plaintext_len, ciphertext_len);
+    EXPECT_EQ(kNumBlocks * blocksize_, ciphertext_len);
     if (rv != CKR_OK) return;
     if (g_verbose) cout << "CT: " << hex_data(ciphertext, ciphertext_len) << endl;
 
     // Now decrypt the data.
-    rv = g_fns->C_DecryptInit(session_, &mechanism, key_.handle());
+    rv = g_fns->C_DecryptInit(session_, &mechanism_, key_.handle());
     EXPECT_CKR_OK(rv);
     if (rv != CKR_OK) return;
 
@@ -53,8 +50,126 @@ class SecretKeyTest : public ReadOnlySessionTest {
     rv = g_fns->C_Decrypt(session_, ciphertext, ciphertext_len, recovered_plaintext, &recovered_plaintext_len);
     if (g_verbose) cout << "PT: " << hex_data(recovered_plaintext, recovered_plaintext_len) << endl;
     EXPECT_CKR_OK(rv);
-    EXPECT_EQ(plaintext_len, recovered_plaintext_len);
-    EXPECT_EQ(0, memcmp(plaintext.get(), recovered_plaintext, plaintext_len));
+    EXPECT_EQ(kNumBlocks * blocksize_, recovered_plaintext_len);
+    EXPECT_EQ(0, memcmp(plaintext_.get(), recovered_plaintext, recovered_plaintext_len));
+  }
+
+  void TestEncryptDecryptParts() {
+    // First encrypt the data block by block.
+    CK_RV rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
+    EXPECT_CKR_OK(rv);
+    if (rv != CKR_OK) return;
+
+    CK_BYTE ciphertext[1024];
+    CK_ULONG ciphertext_bufsize = sizeof(ciphertext);
+    CK_ULONG ciphertext_len = 0;
+    CK_BYTE_PTR part;
+    CK_ULONG part_len;
+    for (int block = 0; block < kNumBlocks; ++block) {
+      part = ciphertext + (block * blocksize_);
+      part_len = ciphertext_bufsize - (part - ciphertext);
+      rv = g_fns->C_EncryptUpdate(session_,
+                                  plaintext_.get() + block * blocksize_, blocksize_,
+                                  part, &part_len);
+      EXPECT_CKR_OK(rv);
+      EXPECT_EQ(blocksize_, part_len);
+      if (rv != CKR_OK) return;
+      if (g_verbose) cout << "CT[" << block << "]: " << hex_data(part, part_len) << endl;
+      ciphertext_len += part_len;
+    }
+    part = ciphertext + (kNumBlocks * blocksize_);
+    part_len = ciphertext_len - (part - ciphertext);
+    EXPECT_CKR_OK(g_fns->C_EncryptFinal(session_, part, &part_len));
+    EXPECT_EQ(0, part_len);
+    ciphertext_len += part_len;
+    EXPECT_EQ(kNumBlocks * blocksize_, ciphertext_len);
+
+    // Now decrypt the data.
+    rv = g_fns->C_DecryptInit(session_, &mechanism_, key_.handle());
+    EXPECT_CKR_OK(rv);
+    if (rv != CKR_OK) return;
+
+    CK_BYTE recovered_plaintext[1024];
+    CK_ULONG recovered_plaintext_bufsize = sizeof(recovered_plaintext);
+    CK_ULONG recovered_plaintext_len = 0;
+    for (int block = 0; block < kNumBlocks; ++block) {
+      part = recovered_plaintext + (block * blocksize_);
+      part_len = recovered_plaintext_bufsize - (part - recovered_plaintext);
+      rv = g_fns->C_DecryptUpdate(session_,
+                                  ciphertext + (block * blocksize_), blocksize_,
+                                  part, &part_len);
+      EXPECT_CKR_OK(rv);
+      EXPECT_EQ(blocksize_, part_len);
+      if (g_verbose) cout << "PT[" << block << "]: " << hex_data(part, part_len) << endl;
+      recovered_plaintext_len += part_len;
+    }
+    part = recovered_plaintext + (kNumBlocks * blocksize_);
+    part_len = recovered_plaintext_bufsize - (part - recovered_plaintext);
+    EXPECT_CKR_OK(g_fns->C_DecryptFinal(session_, part, &part_len));
+    EXPECT_EQ(0, part_len);
+    ciphertext_len += part_len;
+    EXPECT_EQ(kNumBlocks * blocksize_, recovered_plaintext_len);
+
+    EXPECT_EQ(0, memcmp(plaintext_.get(), recovered_plaintext, recovered_plaintext_len));
+  }
+
+  void TestEncryptDecryptErrors() {
+    CK_RV rv;
+    // Various invalid parameters to EncryptInit
+    rv = g_fns->C_EncryptInit(session_, NULL_PTR, key_.handle());
+    EXPECT_TRUE(rv == CKR_ARGUMENTS_BAD  || rv == CKR_MECHANISM_INVALID);
+    CK_MECHANISM mechanism = {CKM_RSA_PKCS, NULL_PTR, 0};
+    rv = g_fns->C_EncryptInit(session_, &mechanism, key_.handle());
+    EXPECT_CKR(CKR_KEY_TYPE_INCONSISTENT, rv);
+
+    rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
+    EXPECT_CKR_OK(rv);
+    rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
+    EXPECT_CKR(CKR_OPERATION_ACTIVE, rv);
+
+    rv = g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, NULL_PTR, NULL_PTR);
+    EXPECT_CKR(CKR_ARGUMENTS_BAD, rv);
+
+    CK_ULONG dummy_len = 0;
+    EXPECT_CKR_OK(g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, NULL_PTR, &dummy_len));
+    EXPECT_EQ(kNumBlocks * blocksize_, dummy_len);
+
+    CK_BYTE buffer[1024];
+    memset(buffer, 0xAB, sizeof(buffer));
+    dummy_len = sizeof(buffer);
+    EXPECT_CKR_OK(g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, buffer, &dummy_len));
+
+    // Start a fresh encryption.
+    EXPECT_CKR_OK(g_fns->C_EncryptInit(session_, &mechanism_, key_.handle()));
+
+    // Try to encrypt into a too-small buffer.
+    memset(buffer, 0xAB, sizeof(buffer));
+    dummy_len = 1;
+    rv = g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, buffer, &dummy_len);
+    EXPECT_CKR(CKR_BUFFER_TOO_SMALL, rv) << "CT: " << hex_data(buffer, dummy_len);
+    EXPECT_EQ(kNumBlocks * blocksize_, dummy_len);
+    EXPECT_EQ(0xAB, buffer[0]);  // Nothing written into buffer
+
+    // Start a fresh encryption.
+    dummy_len = sizeof(buffer);
+    EXPECT_CKR_OK(g_fns->C_Encrypt(session_, plaintext_.get(), kNumBlocks * blocksize_, buffer, &dummy_len));
+    EXPECT_CKR_OK(g_fns->C_EncryptInit(session_, &mechanism_, key_.handle()));
+
+    // Try to encrypt an incomplete block.
+    unique_ptr<CK_BYTE, freer> partial(randmalloc(blocksize_ - 1));
+    CK_ULONG ciphertext_len = sizeof(buffer);
+    rv = g_fns->C_Encrypt(session_, partial.get(), blocksize_ - 1, buffer, &ciphertext_len);
+    EXPECT_TRUE(rv == CKR_DATA_LEN_RANGE || rv == CKR_FUNCTION_FAILED);
+
+    // Now in part-by-part mode.
+    rv = g_fns->C_EncryptInit(session_, &mechanism_, key_.handle());
+    EXPECT_CKR_OK(rv);
+
+    dummy_len = 1;
+    EXPECT_CKR_OK(g_fns->C_EncryptFinal(session_, buffer, &dummy_len));
+    dummy_len = 1;
+    rv = g_fns->C_EncryptFinal(session_, buffer, &dummy_len);
+    EXPECT_CKR(CKR_OPERATION_NOT_INITIALIZED, rv);
   }
 
  private:
@@ -63,6 +178,9 @@ class SecretKeyTest : public ReadOnlySessionTest {
   CK_MECHANISM_TYPE mode_;
   int blocksize_;
   bool emits_iv_;
+  unique_ptr<CK_BYTE, freer> iv_;
+  unique_ptr<CK_BYTE, freer> plaintext_;
+  CK_MECHANISM mechanism_;
 };
 
 
@@ -71,27 +189,27 @@ class DesEcbKeyTest : public SecretKeyTest {
   DesEcbKeyTest(): SecretKeyTest(CKM_DES_KEY_GEN, CKM_DES_ECB, 8, false) {}
 };
 
-TEST_F(DesEcbKeyTest, EncryptDecrypt) {
-  TestEncryptDecrypt();
-}
+TEST_F(DesEcbKeyTest, EncryptDecrypt) { TestEncryptDecrypt(); }
+TEST_F(DesEcbKeyTest, EncryptDecryptParts) { TestEncryptDecryptParts(); }
+TEST_F(DesEcbKeyTest, EncryptDecryptErrors) { TestEncryptDecryptErrors(); }
 
 class DesCbcKeyTest : public SecretKeyTest {
  public:
   DesCbcKeyTest(): SecretKeyTest(CKM_DES_KEY_GEN, CKM_DES_CBC, 8, true) {}
 };
 
-TEST_F(DesCbcKeyTest, EncryptDecrypt) {
-  TestEncryptDecrypt();
-}
+TEST_F(DesCbcKeyTest, EncryptDecrypt) { TestEncryptDecrypt(); }
+TEST_F(DesCbcKeyTest, EncryptDecryptParts) { TestEncryptDecryptParts(); }
+TEST_F(DesCbcKeyTest, EncryptDecryptErrors) { TestEncryptDecryptErrors(); }
 
 class AesCbcKeyTest : public SecretKeyTest {
  public:
   AesCbcKeyTest(): SecretKeyTest(CKM_AES_KEY_GEN, CKM_AES_CBC, 16, true, 16) {}
 };
 
-TEST_F(AesCbcKeyTest, EncryptDecrypt) {
-  TestEncryptDecrypt();
-}
+TEST_F(AesCbcKeyTest, EncryptDecrypt) { TestEncryptDecrypt(); }
+TEST_F(AesCbcKeyTest, EncryptDecryptParts) { TestEncryptDecryptParts(); }
+TEST_F(AesCbcKeyTest, EncryptDecryptErrors) { TestEncryptDecryptErrors(); }
 
 }  // namespace test
 }  // namespace pkcs11
